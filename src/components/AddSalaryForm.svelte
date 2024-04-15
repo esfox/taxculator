e738d574bc3c837b676c72dd82fb0df706bc1d1e
esfox <osairis100@gmail.com>
2024-04-16 01:57:29 +0800

    Remove deductions in salary form

--- src/components/AddSalaryForm.svelte
+++ src/components/AddSalaryForm.svelte
@@ -11,17 +11,8 @@
 
   const currentLocalDate = new Date().toISOString().slice(0, 10);
 
-  let deductions: number[] = [];
   let saving = false;
   let formResult = {} as ActionData;
-
-  const addDeduction = () => {
-    deductions = [...deductions, deductions.length];
-  };
-
-  const removeDeduction = (index: number) => {
-    deductions = deductions.filter((_, i) => i !== index);
-  };
 
   const onSubmit: SubmitFunction = () => {
     saving = true;
@@ -79,58 +70,7 @@
     </div>
   </div>
 
-  <div class="mt-6">
-    <h5>Deductions</h5>
-    {#each deductions as i (i)}
-      <div class="flex flex-col items-end bg-neutral-800 p-4 my-4">
-        <div class="form-control w-full mt-2">
-          <label for="amount" class="label">
-            <span class="label-text">Amount</span>
-          </label>
-          <div class="input-group">
-            <span>₱</span>
-            <!-- svelte-ignore a11y-autofocus -->
-            <input
-              id={`deduction-amount-${i}`}
-              name={`deductions[${i}].amount`}
-              type="number"
-              class="input input-bordered"
-              autofocus
-              required
-            />
-          </div>
-        </div>
-        <div class="form-control w-full mt-2">
-          <label for="amount" class="label">
-            <span class="label-text">Description</span>
-          </label>
-
-          <input
-            id={`deduction-description-${i}`}
-            name={`deductions[${i}].description`}
-            type="text"
-            class="input input-bordered"
-            required
-          />
-        </div>
-        <button
-          class="btn btn-error btn-sm mt-4"
-          on:click|preventDefault={() => removeDeduction(i)}
-        >
-          <i class="fa-solid fa-trash" />
-          Remove
-        </button>
-      </div>
-    {/each}
-    <button class="btn btn-primary mt-2" on:click|preventDefault={addDeduction}>
-      <i class="fa-solid fa-plus" />
-      Add
-    </button>
-  </div>
   <hr class="border-neutral-600 mt-6" />
-  {#if formResult?.exceedingDeductions}
-    <div class="text-center text-error mt-2">Deductions cannot exceed the salary amount</div>
-  {/if}
   <div class="flex justify-center mt-6">
     {#if !saving}
       <button class="btn btn-secondary w-32" on:click|preventDefault={onCancel}>
